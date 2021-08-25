8e87bbf88eb91d7956f030ac28eaa1cf8da860fb
kitty-panics <e222d29f@outlook.com>
2021-08-25 10:17:23 +0800

    docs: 更新 official 文档下载地址

--- official/get-docs-list.sh
+++ official/get-docs-list.sh
@@ -11,15 +11,20 @@ SET_TMP_PATH=$(mktemp -d)
 git clone --depth=1 git@github.com:Kapeli/feeds.git "$SET_TMP_PATH/feeds"
 
 # 过滤出文档名字
-SET_DATE=$(date +'%Y-%m-%d')
+#SET_DATE=$(date +'%Y-%m-%d')
 ls "$SET_TMP_PATH/feeds" | cat | sed '/README.md/d' | while read -r DOCS
 do
     # 输出下载链接
-    grep "sanfrancisco" "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-sanfrancisco-$SET_DATE.txt
-    grep "london"       "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-london-$SET_DATE.txt
-    grep "newyork"      "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-newyork-$SET_DATE.txt
-    grep "tokyo"        "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-tokyo-$SET_DATE.txt
-    grep "frankfurt"    "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-frankfurt-$SET_DATE.txt
+    #grep "sanfrancisco" "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-sanfrancisco-$SET_DATE.txt
+    #grep "london"       "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-london-$SET_DATE.txt
+    #grep "newyork"      "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-newyork-$SET_DATE.txt
+    #grep "tokyo"        "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-tokyo-$SET_DATE.txt
+    #grep "frankfurt"    "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-frankfurt-$SET_DATE.txt
+    grep "sanfrancisco" "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-sanfrancisco.txt
+    grep "london"       "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-london.txt
+    grep "newyork"      "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-newyork.txt
+    grep "tokyo"        "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-tokyo.txt
+    grep "frankfurt"    "$SET_TMP_PATH/feeds/$DOCS" | sed 's/^    <url>//g' | sed 's/<\/url>$//g' >> ../official-frankfurt.txt
 done
 
 # 销毁临时环境
